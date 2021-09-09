18a31f9e3117944c396a29f318cc47a966a72017
ergunmu1 <mustafa.ergun@natixis.com>
2021-09-09 10:39:30 +0200

    first commit

---
+++ src/main/java/com/natixis/chatdonback/ChatdonBackApplication.java
@@ -0,0 +1,13 @@
+package com.natixis.chatdonback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatdonBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatdonBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/natixis/chatdonback/ChatdonBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natixis.chatdonback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatdonBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
